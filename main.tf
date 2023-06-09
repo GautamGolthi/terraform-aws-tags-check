4688a4f9959838c7c44a0d113c89b456294f771d
CloudSihmar <105888131+CloudSihmar@users.noreply.github.com>
2023-06-09 12:28:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2_example" {
       sudo apt-get update -u
       sudo apt install -y nginx
       sudo systemctl start nginx
-      sudo echo "<html><body><h1>Hello this is test module1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
+      sudo echo "<html><body><h1>Hello this is cloud module1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
       EOF
 
 tags = {
