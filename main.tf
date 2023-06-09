128f89b1a1ce83ef9088e26c877b3c58694610df
GautamGolthi <30389900+GautamGolthi@users.noreply.github.com>
2023-06-09 13:16:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,10 +17,6 @@ resource "aws_instance" "ec2_example" {
       sudo echo "<html><body><h1>Hello this is cloud module1 at instance id `curl http://169.254.169.254/latest/meta-data/instance-id` </h1></body></html>" > /var/www/html/index.html
       EOF
 
-tags = {
- 
- Name = "sentinel-server"
-}
 
 }
 
